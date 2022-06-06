16492b2f2c93dea230658426e99d2f859b76247c
Marcelo Pallone <marcelo.pallone@gmail.com>
2022-06-06 17:46:28 -0300

    voltando atrás e apagando os logs, para teste

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao bando de dados");
-// mais um console log de teste
-console.log("o marcelo fez esse commit, estamos em testes!");
-console.log("Enviando para o GitHub");
 
